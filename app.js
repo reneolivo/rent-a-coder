9b792528e4c88d2e7056e9a9ecdb2d384f0daa6b
Rene Olivo <minet.ws@gmail.com>
2014-06-07 15:53:16 -0400

    add less middleware

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express         = require('express');
+var path            = require('path');
+//var favicon         = require('static-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var less            = require('less-middleware');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes          = require('./routes/index');
+var users           = require('./routes/users');
 
-var app = express();
+var app             = express();
+
+var bootstrapPath   = path.join(__dirname, 'public', 'bower_components', 'bootstrap');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.locals.pretty = true
+
+app.use(less(path.join(__dirname, 'public/stylesheets', 'less'), {
+    paths   : [ path.join(bootstrapPath, 'less') ],
+    dest    : path.join(__dirname, 'public', 'stylesheets'),
+    prefix  : '/stylesheets',
+    debug   : true
+}));
 
 app.use('/', routes);
 app.use('/users', users);
