3279506d071a4700f60323d9f59d115b16752294
Rene Olivo <minet.ws@gmail.com>
2014-06-08 16:02:01 -0400

    add mock data

--- data/mock-data.js
+++ data/mock-data.js
@@ -1,7 +1,7 @@
 var coders		= [
 	{
 		name		: 'Pearl Stewart',
-		email		: 'pearl.stewart61@example.com',
+		email		: 'p.stewart61@example.com',
 		phone		: '(823)-164-6573',
 		address		: '3466 Robinson Rd',
 		imagePath	: 'random-person-1.jpg',
@@ -10,7 +10,7 @@ var coders		= [
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
 Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
 		',
-		price		: 9.99,
+		price		: 19.99,
 		skills		: [
 			'HTML',
 			'CSS',
@@ -21,139 +21,148 @@ Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Francisco Matthews',
+		email		: 'f.matthews14@example.com',
+		phone		: '(895)-975-2106',
+		address		: '7948 Pine Rd',
+		imagePath	: 'random-person-2.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 21.99,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'SWIFT',
+			'AS3',
+			'Android SDK'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Roland Phillips',
+		email		: 'r.phillips27@example.com',
+		phone		: '(839)-624-2271',
+		address		: '7993 W Campbell St',
+		imagePath	: 'random-person-3.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 15.10,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'.NET',
+			'XAMARIN',
+			'JavaScript'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Amber Watson',
+		email		: 'a.watson35@example.com',
+		phone		: '(604)-369-1504',
+		address		: '7655 Wayne Rd',
+		imagePath	: 'random-person-4.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 18.50,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'Java',
+			'Oracle',
+			'SQL Server'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Sally Day',
+		email		: 's.day41@example.com',
+		phone		: '(150)-621-4132',
+		address		: '3563 Hinckley Blvd',
+		imagePath	: 'random-person-5.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 14.30,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'NodeJS',
+			'Socket.io',
+			'Express'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Felix Wagner',
+		email		: 'f.wagner92@example.com',
+		phone		: '(123)-648-1683',
+		address		: '1258 E Broadway St',
+		imagePath	: 'random-person-6.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 22.99,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'MongoDB',
+			'Neo4j',
+			'Redis'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Herman Day',
+		email		: 'herman.day54@example.com',
+		phone		: '(783)-815-4088',
+		address		: '8701 Hobbs Dr',
+		imagePath	: 'random-person-7.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 21.50,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'HTML',
+			'CSS',
+			'JavaScript',
+			'PHP'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Marcia Hicks',
+		email		: 'm.hicks41@example.com',
+		phone		: '(266)-243-8415',
+		address		: '5271 Airport St',
+		imagePath	: 'random-person-8.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 17.92,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'Python',
+			'dJango',
+			'MongoDB'
 		]
 	},
 	{
-		name		: '',
-		email		: '',
-		phone		: '',
-		address		: '',
-		imagePath	: '',
-		resume		: '',
-		bio			: '',
-		price		: 1,
+		name		: 'Luis Lambert',
+		email		: 'l.lambert58@example.com',
+		phone		: '(633)-170-9373',
+		address		: '1930 Washington Rd',
+		imagePath	: 'random-person-9.jpg',
+		resume		: 'Phasellus purus urna, sodales ac ligula nec, ultricies malesuada lacus.',
+		bio			: '\
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet nibh nunc. Nulla eleifend, lorem ut sodales interdum, ipsum turpis sodales lectus, eget rutrum orci ligula ac urna.</p>\
+Proin 		<p>euismod dui ut mauris egestas tempor nec quis nibh. In aliquam at leo eu rhoncus. Ut iaculis at est id scelerisque. Fusce dignissim mattis arcu ut tincidunt. Donec molestie nunc ut lectus gravida pulvinar.</p>\
+		',
+		price		: 13.50,
 		skills		: [
-			'',
-			'',
-			'',
-			'',
-			''
+			'Ruby',
+			'Rails',
+			'MySQL'
 		]
 	}
 ];
